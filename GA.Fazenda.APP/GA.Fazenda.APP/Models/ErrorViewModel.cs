fa02ef1f2e7cce2d6912d44b9972caf2236031d9
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2022-03-15 22:51:48 -0300

    Startup do Projeto CRUD Fazenda
    Startup do Projeto CRUD Fazenda

---
+++ GA.Fazenda.APP/GA.Fazenda.APP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GA.Fazenda.APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
